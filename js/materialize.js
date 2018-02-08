c1914ae7a4c7abc663c6e40fd6d19f9d32c414bb
GirlLollipop <girl.lollipop@gmail.com>
2018-02-08 12:49:16 -0600

    actualizado

--- js/materialize.js
+++ js/materialize.js
@@ -3,3 +3,6 @@ $(document).ready(function(){
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
   });
+
+  //menú collapse
+$(".button-collapse").sideNav();
