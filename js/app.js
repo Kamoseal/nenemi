84ca010e0f1d825af5e7c6d786c49cc7d4c25952
GirlLollipop <girl.lollipop@gmail.com>
2018-02-08 20:22:29 -0600

    upload picture

--- js/app.js
+++ js/app.js
@@ -50,3 +50,63 @@ firebase.database().ref("nenemi")
  var user = s.val();
 
 });
+
+
+window.onload = start;
+
+var photo = document.getElementById('upload');
+var storageRef;
+var img;
+var result = document.getElementById('uploadPhoto');
+
+
+function start() {
+photo.addEventListener('change', uploadimg, false);
+ // referencia a la raiz de storage
+ storageRef = firebase.storage().ref();
+ // referecia a la base de datos
+ img = firebase.database().ref().child("images");
+ showImages();
+
+function showImages(){
+ img.on("value",function(snapshot){
+    var datos = snapshot.val();
+    for(var key in datos){
+     var dataUrl = datos[key].url;
+
+     // var result = "<img width='200px'  + datos[key].url />";
+   };
+    result.innerHTML = "<img width='200px' src = '"+ dataUrl +"' />"
+
+
+    // document.getElementById('uploadPhoto').innerHTML = result;
+ });
+};
+
+function uploadimg() {
+ var imgUp = photo.files[0];
+ var uploadTask = storageRef.child('images/' + imgUp.name).put(imgUp);
+ // document.getElementById('progress').className = ' ';
+ uploadTask.on('state_changed', function(snapshot){
+ // var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+ // se necesita id de la barra de progreso
+ // document.getElementById('id').style.width = progress + "%";
+}, function(error) {
+ // Gestionar el error
+  alert('error');
+}, function() {
+ // Cuando se hasubido exitosamente
+ var downloadURL = uploadTask.snapshot.downloadURL;
+ console.log(downloadURL);
+ // Crear nodo en base de datos firebaseio
+ alert('foto subida con exito');
+ crearNodoEnBDfirebase(imagenASubir.name, downloadURL);
+ // document.getElementById('progress').className = hide;
+});
+};
+
+function crearNodoEnBDfirebase(uploadTask, downloadURL){
+ imagenes.push({ nombre:uploadTask, url:downloadURL})
+};
+
+};
